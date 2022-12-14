481a17fb47a865f670e0441f5000e105046d1784
NileshSaha <03.nileshsaha@gmail.com>
2022-12-14 21:58:15 +0530

    feedback stat added

---
+++ src/components/FeedbackStats.jsx
@@ -0,0 +1,39 @@
+import PropTypes from 'prop-types'
+
+function FeedbackStats({ feedback }) {
+  let average =
+    feedback.reduce((acc, cur) => {
+      return acc + cur.rating
+    }, 0) / feedback.length
+
+  average = average.toFixed(1).replace(/[.,]0$/, '')
+
+  return (
+    <div className='feedback-stats'>
+      <h4>{feedback.length} Reviews</h4>
+      <h4>Average Rating:{isNaN(average) ? 0 : average}</h4>
+    </div>
+  )
+}
+
+FeedbackStats.defaultProps = {
+  feedback: [
+    {
+      id: 1,
+      rating: 10,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+  ],
+}
+
+FeedbackStats.propTypes = {
+  feedback: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      rating: PropTypes.number.isRequired,
+      text: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+}
+
+export default FeedbackStats
