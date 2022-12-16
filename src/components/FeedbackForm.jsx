a44bea92e314e7353052b497f2e98d8537040994
NileshSaha <03.nileshsaha@gmail.com>
2022-12-17 00:38:08 +0530

    text length validation added

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,16 +1,28 @@
 import { useState } from 'react'
+import Button from './shared/Button'
 import Card from './shared/Card'
 
 function FeedbackForm() {
   const [text, setText] = useState('')
+  const [btnDisabled, setBtnDisabled] = useState(true)
+  const [message, setMessage] = useState('')
   const handleTextChange = (e) => {
+    if (text === '') {
+      setBtnDisabled(true)
+      setMessage(null)
+    } else if (text.trim().length > 10) {
+      setBtnDisabled(false)
+      setMessage(null)
+    } else {
+      setBtnDisabled(true)
+      setMessage('Minimum 10 characters are required')
+    }
     setText(e.target.value)
   }
   return (
     <Card>
       <form>
         <h2>How would you rate my service?</h2>
-
         <div className='input-group'>
           <input
             type='text'
@@ -18,8 +30,11 @@ function FeedbackForm() {
             placeholder='Write a review...'
             value={text}
           />
-          <button type='submit'>Send</button>
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   )
